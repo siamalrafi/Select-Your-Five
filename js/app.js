3f2bdcd5ac884b7b819f4559979158baf23be306
RAHAT HASAN DIPO <rahathasandipto@gmail.com>
2022-08-22 04:33:49 +0600

    This is the next commit

--- js/app.js
+++ js/app.js
@@ -1,1 +1,27 @@
-console.log('This isisisi')
+let playerArry = [];
+function setValue(playerArry) {
+    const setPlayer = document.getElementById('player-list');
+    setPlayer.innerHTML = '';
+    for (let i = 0; i < playerArry.length; i++) {
+        const playerName = playerArry[i].player;
+        console.log(playerName);
+        const tr = document.createElement('tr');
+        tr.innerHTML = `
+        <th>${i + 1}</th>
+        <td>${playerName}</td> `;
+        setPlayer.appendChild(tr);
+    }
+}
+
+function clickBtn(element) {
+    let player = (element.parentNode.parentNode.children[0].innerText);
+    const playerObj = {
+        player: player,
+    }
+    playerArry.push(playerObj);
+    let selectedPlayer = playerArry.length;
+    if (selectedPlayer >= 6) {
+        alert('You already select five player')
+    }
+    setValue(playerArry)
+}
